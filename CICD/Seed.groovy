2e9192630aeb4c95eb7ac8ca28f199b1ccf37957
other019 <rzepkafoto@gmail.com>
2020-05-03 00:16:08 +0200

    move seed

---
+++ CICD/Seed.groovy
@@ -0,0 +1,19 @@
+def repo_url = 'https://github.com/other019/mrchecker-source.git'
+multibranchPipelineJob('build'){
+	description("Build source code and provide packages")
+	branchSources{
+		git{
+			//id('12314')
+			remote(repo_url)
+		}
+		//github is a valid option too
+	}
+	orphanedItemStratedy{
+		discardOldItems{
+			daysToKeep(30)
+		}
+	}
+	triggers{
+		cron("")
+	}
+}
