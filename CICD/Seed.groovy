bbe15baa05aa80af8a69e0f1e1c4a15aeb4d84ed
other019 <rzepkafoto@gmail.com>
2020-05-03 00:28:47 +0200

    added node, specified id

--- CICD/Seed.groovy
+++ CICD/Seed.groovy
@@ -1,21 +1,24 @@
-jobDsl scriptText: """
-def repo_url = 'https://github.com/other019/mrchecker-source.git'
-multibranchPipelineJob('build'){
-	description("Build source code and provide packages")
-	branchSources{
-		git{
-			//id('12314')
-			remote(repo_url)
+
+node{
+	jobDsl scriptText: """
+	def repo_url = 'https://github.com/other019/mrchecker-source.git'
+	multibranchPipelineJob('build'){
+		description("Build source code and provide packages")
+		branchSources{
+			git{
+				id('12314')
+				remote(repo_url)
+			}
+			//github is a valid option too
 		}
-		//github is a valid option too
-	}
-	orphanedItemStratedy{
-		discardOldItems{
-			daysToKeep(30)
+		orphanedItemStratedy{
+			discardOldItems{
+				daysToKeep(30)
+			}
+		}
+		triggers{
+			cron("")
 		}
 	}
-	triggers{
-		cron("")
-	}
+	"""
 }
-"""
