c448eb53210e436b0b642e3aac626affcc5efda5
other019 <rzepkafoto@gmail.com>
2020-05-17 23:46:39 +0200

    add seed job

--- CICD/Seed.groovy
+++ CICD/Seed.groovy
@@ -1,12 +1,22 @@
+def script = makeJobs()
+print '\n'*5+'-'*80+'\n\t\t\tSCRIPT\n'
+print script
+print '\n'*5+'-'*80+'\n\t\t\tEND\n'
 node{
-	jobDsl scriptText: """
+	jobDsl scriptText: script
+}
+
+@NonCPS
+def makeJobs(){
+	def modules = ['mrchecker-core-module','mrchecker-database-module','mrchecker-example-module','mrchecker-mobile-module','mrchecker-security-module','mrchecker-selenium-module','mrchecker-webapi-module']
 	def repo_url = 'https://github.com/other019/mrchecker-source.git'
-	multibranchPipelineJob('build'){
+	def jobTemplateText = """
+	multibranchPipelineJob("\${module}"){
 		description("Build source code and provide packages")
 		branchSources{
 			git{
 				id('12314')
-				remote(repo_url)
+				remote('${repo_url}')
 			}
 		}
 		orphanedItemStrategy{
@@ -24,4 +34,11 @@ node{
 		}
 	}
 	"""
+	def script = ''
+	def engine = new groovy.text.SimpleTemplateEngine()
+	def jobTemplate = engine.createTemplate(jobTemplateText)
+	modules.each{
+		script += jobTemplate.make([module:it])
+	}
+	return script
 }
